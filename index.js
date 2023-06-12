5a2718965625e92d3dd493dca13ba747bef5fcc1
Eunbyul <las4134@gmail.com>
2023-06-12 08:33:14 +0100

    Add static map api

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+//API key (We need to hide it later!)
 const ApiKey =  '666dbb9c6137456690cfd79fe7ac0563';
 
 const autocompleteInput = new autocomplete.GeocoderAutocomplete(
     document.getElementById("autocomplete"), 
-    //API key (We need to hide it later!)
-    '666dbb9c6137456690cfd79fe7ac0563', 
+    ApiKey,
     { /* Geocoder options */ });
 
 autocompleteInput.on('select', (location) => {
-// check selected location here,
-console.log(location)
+// latitude, longitude value
 const { properties:{lat,lon} } = location
 
-console.log(lat, lon)
-
-
+fetch(`https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:${lon},${lat}&zoom=14&marker=lonlat:${lon},${lat};color:%23ff0000;size:medium&apiKey=${ApiKey}`)
+.then((res)=>{
+    if(!res.ok) throw new Error(res.status)
+    return res;
+})
+.then((data)=>{
+    let {url} = data
+    let mapImg = document.querySelector('#mapImg')
+    mapImg.src= data.url
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 });
 
