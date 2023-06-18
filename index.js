5b2e6a951e60c88fb5d10f81d6dee36f29ece32f
Hex <hexmusictheory@protonmail.com>
2023-06-18 03:25:28 +0100

    feat: add results display

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const autocompleteInput = new autocomplete.GeocoderAutocomplete(
     ApiKey,
     { /* Geocoder options */ });
 
+// variables to store data each time the API is called
+let sunrise, sunset, dayLength, noon;
+// number of seconds in a day
+const DAY_SECONDS = 24 * 3600;
+// grab the divs to show/hide on making a query
+const dataText = document.querySelector('.sunset-times');
+const barContainer = document.querySelector('.bar-container');
+// grab the 3 divs forming the 3-segment bar display
+const morningBar = document.querySelector('.morning-bar');
+const dayBar = document.querySelector('.day-bar');
+const nightBar = document.querySelector('.night-bar');
+// grab the span elements to print results in
+const sunriseText = document.querySelector('.sunrise-text');
+const sunsetText = document.querySelector('.sunset-text');
+const noonText = document.querySelector('.noon-text');
+const lengthText = document.querySelector('.length-text');
+
 autocompleteInput.on('select', (location) => {
-  
+
     // latitude, longitude value
     const { properties: { lat, lon } } = location
 
@@ -29,7 +46,7 @@ autocompleteInput.on('select', (location) => {
             //Hide loader display
             loader.style.display = 'none'
         })
-        .catch((err)=>{
+        .catch((err) => {
             const errMsg = document.querySelector('#err-msg')
             loader.style.display = 'none';
             errMsg.style.display = 'block'
@@ -41,7 +58,22 @@ autocompleteInput.on('select', (location) => {
             if (!res.ok) throw new Error(res.status)
             return res.json();
         })
-        .then(res => console.log(res))
+        .then(res => res.results)
+        .then(data => {
+            sunrise = data.sunrise;
+            sunset = data.sunset;
+            dayLength = data.day_length;
+            noon = data.solar_noon;
+            sunriseText.innerHTML = sunrise;
+            sunsetText.innerHTML = sunset;
+            noonText.innerHTML = noon;
+            lengthText.innerHTML = dayLength;
+            morningBar.style.width = (toSeconds(sunrise) * 100 / DAY_SECONDS) + '%';
+            dayBar.style.width = ((toSeconds(sunset) - toSeconds(sunrise)) * 100 / DAY_SECONDS) + '%';
+            nightBar.style.width = ((DAY_SECONDS - toSeconds(sunset)) * 100 / DAY_SECONDS) + '%';
+            dataText.style.display = 'block';
+            barContainer.style.display = 'flex';
+        })
 
 });
 
@@ -50,8 +82,24 @@ autocompleteInput.on('suggestions', (suggestions) => {
 
 });
 
-
-
-
-
-
+// function to convert time strings into seconds from 00:00:00 AM
+function toSeconds(time) {
+    let seconds, minutes, hours;
+    const digits = time.replace(/[\D]/g, '').split('');
+    let temp = digits.pop();
+    seconds = digits.pop() + temp;
+    temp = digits.pop();
+    minutes = digits.pop() + temp;
+    if (digits.length === 2) {
+        temp = digits.pop();
+        hours = digits.pop() + temp;
+    } else {
+        hours = digits.pop();
+    }
+    hours = parseInt(hours) * 3600;
+    minutes = parseInt(minutes) * 60;
+    seconds = parseInt(seconds);
+    let total = /PM/.test(time) ? 12 * 3600 : 0;
+    total += hours + minutes + seconds;
+    return total;
+}
